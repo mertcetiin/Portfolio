25822a1a6b9ebd47e218e179f625b2018f13d01d
mertcetiin <mrtctn506@gmail.com>
2023-10-28 12:53:45 +0300

    router loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
 
 
   return (
     <div>
-
+      <Routes>
+        <Route path='' element={<Home />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Home;
