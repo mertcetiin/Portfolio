d1458129943e97dcc1cf314a004e7b2d159ff910
mertcetiin <mrtctn506@gmail.com>
2023-11-04 12:03:25 +0300

    .

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,31 +2,8 @@
 
 function ContactForm() {
     return (
-        <div className="w-full">
-            <div className="flex flex-col mt-60 justify-center max-w-md mx-auto bg-white shadow-xl rounded-xl p-5">
-                <form className="bg-white p-4 rounded-lg shadow-lg">
-                    <div className="mb-4">
-                        <input type="text" className="w-full py-2 px-3 rounded border" placeholder="Email" name="email" required />
-                    </div>
-
-                    <div className="mb-4">
-                        <input type="text" className="w-full py-2 px-3 rounded border" placeholder="Name" name="name" />
-                    </div>
-
-                    <div className="mb-4">
-                        <input type="text" className="w-full py-2 px-3 rounded border" placeholder="Subject" name="subject" required />
-                    </div>
-
-                    <div className="mb-4">
-                        <textarea className="w-full py-2 px-3 rounded border" placeholder="Message" name="message" required />
-                    </div>
-
-                    <div className="mb-4">
-                        <button type="submit" className="bg-gray-900 text-white py-2 px-4 rounded hover:bg-blue-800" name="subject">
-                            Send Message</button>
-                    </div>
-                </form>
-            </div>
+        <div>
+            <h1>contactForm</h1>
         </div>
     )
 }
