f17409335121c410142679fba082387287cb3ec6
mertcetiin <mrtctn506@gmail.com>
2023-11-06 23:26:52 +0300

    contact bug fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import cv from '../assets/cv.pdf';
+import { Link } from "react-router-dom";
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
@@ -46,10 +47,10 @@ function Header() {
                         </li>
 
                         <li>
-                            <a href="/contact"
+                            <Link to="/contact"
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover-bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Contact
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
