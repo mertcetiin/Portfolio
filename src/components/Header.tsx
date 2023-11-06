cd58a84fc6f344ebba0acac9e50ae4624e66e2c2
mertcetiin <mrtctn506@gmail.com>
2023-11-06 23:21:05 +0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ function Header() {
                             </a>
                         </li>
                         <li>
-                            <a href="/MertCetin-Resume.pdf" download
+                            <a href="/public/cv.pdf" download
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover-bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Resume
                             </a>
