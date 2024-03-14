f4c224fcb7032e3048d318ee9d68e3fe20ebb994
mertcetiin <mrtctn506@gmail.com>
2024-03-14 13:31:45 +0300

    resume loaded

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import mertcv from '../assets/mertcv.pdf';
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -40,7 +39,7 @@ function Header() {
                             </a>
                         </li>
                         <li>
-                            <a href={mertcv} download
+                            <a href='/mertcv.pdf' download
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover-bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Resume
                             </a>
